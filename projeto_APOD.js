fc6420b9e9da02c9a55686a666afaf5adc957508
viviancarolinemiranda <viviancaroliner@gmail.com>
2021-12-15 14:42:06 -0300

    primeiro commit

--- projeto_APOD.js
+++ projeto_APOD.js
@@ -1,12 +1,22 @@
 let btn = $("#enviar");
-btn.addEventListener("click", function () {
-$.ajax({
-  url: "https://api.nasa.gov/planetary/apod?api_key=BbFITBloga1tY0wVkNb7zK0cIvyYZu6X7JBvAzX8",
-  success: (result) => {
-    apod(result);
-  },
-  error: (error) => {
-    console.error(error);
-  },
+let titulo = $("#titulo");
+let data = $("#data");
+let imagem = $("#imagem");
+let autor = $("#autor");
+let descricao = $("#descricao");
+btn.on("click", function () {
+  let date = $("inputData").val();
+  $.ajax({
+    url: `https://api.nasa.gov/planetary/apod?api_key=qBRvxU4X64VGeTw3Be7WWP2dyPskabtYloHQp1gadate=${date}`,
+    type: "GET",
+
+    success: function (result) {
+      titulo.html(`${result.title}`);
+      date.html(`${result.date}`);
+      console.log(result);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
 });
-}
