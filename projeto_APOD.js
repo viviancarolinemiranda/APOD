6673d072011bab8bb5f9f2009e9dacf21bf930e0
viviancarolinemiranda <viviancaroliner@gmail.com>
2021-12-15 15:40:10 -0300

    finalização js

--- projeto_APOD.js
+++ projeto_APOD.js
@@ -4,16 +4,25 @@ let data = $("#data");
 let imagem = $("#imagem");
 let autor = $("#autor");
 let descricao = $("#descricao");
+let video = $("#video");
 btn.on("click", function () {
-  let date = $("inputData").val();
+  let inputData = $("#input-data").val();
   $.ajax({
-    url: `https://api.nasa.gov/planetary/apod?api_key=qBRvxU4X64VGeTw3Be7WWP2dyPskabtYloHQp1gadate=${date}`,
+    url: `https://api.nasa.gov/planetary/apod?api_key=qBRvxU4X64VGeTw3Be7WWP2dyPskabtYloHQp1ga&date=${inputData}`,
     type: "GET",
 
     success: function (result) {
       titulo.html(`${result.title}`);
-      date.html(`${result.date}`);
-      console.log(result);
+      data.html(`${result.date}`);
+      autor.html(`By: ${result.copyright}`);
+      descricao.html(`${result.explanation}`);
+      if (result.media_type == "image") {
+        imagem.attr("src", result.url);
+      } else {
+        document.getElementById("imagem").style.display = "none";
+        document.getElementById("video").style.display = "flex";
+        video.attr("src", result.url);
+      }
     },
     error: function (error) {
       console.log(error);
